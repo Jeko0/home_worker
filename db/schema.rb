562b23abd00d90fe03474f0f3a846709acdacb70
Jeko0 <japulava@unisens.ge>
2022-09-09 10:56:23 +0400

    implemented categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_113956) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_060648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "subject_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_categories_on_subject_id"
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string "title"
@@ -33,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_113956) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "subjects"
 end
