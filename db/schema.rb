52fbe58a0dfbcef826ca205b350b0c8c2410c3df
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-09-09 12:44:50 +0400

    scema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_09_09_060648) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_061328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_060648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["subject_id"], name: "index_categories_on_subject_id"
-
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti"
@@ -60,6 +59,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_060648) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "writers", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.float "rating", default: 0.0
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_writers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_writers_on_reset_password_token", unique: true
+  end
+
   create_table "writers_infos", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.float "rating"
@@ -71,7 +84,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_060648) do
     t.index ["user_id"], name: "index_writers_infos_on_user_id"
   end
 
+  add_foreign_key "categories", "subjects"
+  add_foreign_key "reviews", "users"
   add_foreign_key "writers_infos", "subjects"
   add_foreign_key "writers_infos", "users"
-  add_foreign_key "reviews", "users"
 end
