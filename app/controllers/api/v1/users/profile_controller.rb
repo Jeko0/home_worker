8f1ed7a021c8c3f2565a6c4efad82064a29801a4
= <=>
2022-09-12 12:34:09 +0400

    added user serializer

--- app/controllers/api/v1/users/profile_controller.rb
+++ app/controllers/api/v1/users/profile_controller.rb
@@ -4,13 +4,12 @@ module Api
       def index
         @users = User.all
 
-        render json: @users.map { |user| user.username }, status: 302
+        render json: @users, status: 302
       end
 
       def show
         @user = User.find_by(id: params[:id])
-
-        render json: @user.username
+        render json: @user
       end
     end
   end
