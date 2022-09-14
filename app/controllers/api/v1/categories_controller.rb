4f34a1b8d61b1381741374199d0b4d5e2ca7ad84
= <=>
2022-09-14 11:58:09 +0400

    added_subject_categories_method

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::CategoriesController < ApplicationController
     @categories = Category.all 
     render json: @categories
   end
+
+  def subject_categories
+    @subject = Subject.find_by(id: params[:subject_id])
+    if @subject
+      @subject_category = @subject.categories
+      render json: @subject_category
+    else
+      render json: { error: "Subject not found" }
+    end
+  end
 end
