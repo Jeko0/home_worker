b8391583554346ce6791f40b01307f510a52b5fa
Jeko0 <japulava@unisens.ge>
2022-09-15 17:44:04 +0400

    added carousel slide and page header

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Links } from '../data/links';
-import { slide as Menu } from 'react-burger-menu';
+import React, { Fragment } from "react";
+import { FaBookOpen } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { Links } from "../data/links";
+import { slide as Menu } from "react-burger-menu";
 
 export const Sidebar = () => {
-  const sidebarStyle = "items-center gap-3 ml-3 mt-4 flex text-x1 front-extrabold tracking-tight dark:text-white text-slate-900 pl-3 hover:bg-slate-400 hover:rounded-lg"
+  const sidebarStyle =
+    "items-center gap-3 ml-3 mt-4 flex text-xl front-extrabold tracking-tight dark:text-white text-slate-900 pl-3 hover:bg-slate-400 hover:rounded-lg";
   return (
     <Menu>
-      <div className='flex justify-between items-center'>
+      <div className="flex justify-between items-center">
+        <div className=" flex text-2xl gap-3 items-center  ml-3 mt-4  front-extrabold tracking-tight dark:text-white text-slate-900 ">
+          <FaBookOpen /> <span>Home Worker</span>
+        </div>
+      </div>
+      <div className="flex justify-between items-center">
         {Links.map((item) => (
           <div key={item.title}>
-            <p className='text-black m-3 mt-4'>
-              { item.title }
-            </p>   
+            <p className="text-black m-3 mt-4">{item.title}</p>
 
             {item.links.map((link) => (
-              <NavLink to={`/${link.name}`} key={link.name} className={sidebarStyle} >
-                { link.icon }
-                <span className='capitalize'>
-                  { link.name }
-                </span>
+              <NavLink
+                to={`/${link.name}`}
+                key={link.name}
+                className={sidebarStyle}
+              >
+                {link.icon}
+                <span className="capitalize">{link.name}</span>
               </NavLink>
-             ))}
+            ))}
           </div>
         ))}
       </div>
     </Menu>
-  )
-}
+  );
+};
--- frontend/src/routes/home/home.jsx
+++ frontend/src/routes/home/home.jsx
@@ -1,12 +1,56 @@
-import './home.styles.scss'
-
+import React from "react";
+import "./home.styles.scss";
+import step from "../../files/images/step.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Carousel, { CarouselItem } from "../../components/Carousel";
+import math from "../../files/images/Math.jpg";
+import english from "../../files/images/English.webp";
+import geography from "../../files/images/Geography.webp";
+import history from "../../files/images/History.webp";
+import chemistry from "../../files/images/Chemistry.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="overflow-hidden">
+      <div
+        className="w-full 
+      bg-cover 
+      bg-center  
+      hover:scale-110 
+      ease-in duration-500 "
+        style={{ height: "25rem", backgroundImage: `url(${step})` }}
+      >
+        <div
+          className="flex 
+        items-center 
+        justify-center 
+        h-full 
+        w-full 
+        bg-opacity-50"
+        >
+          <h1
+            className="
+          text-gray-200 
+          text-2xl 
+          font-semibold 
+          uppercase 
+          md:text-3xl"
+          >
+            Writing Services For Your Needs
+          </h1>
+        </div>
+      </div>
+
+      <div className="mt-12">
+        <Carousel>
+          <CarouselItem>1</CarouselItem>
+          <CarouselItem>2</CarouselItem>
+          <CarouselItem>3</CarouselItem>
+          <CarouselItem>4</CarouselItem>
+        </Carousel>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
